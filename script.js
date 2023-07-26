9059f710914a99ad016ec2be8ea5c8c2d6de6d94
riho5 <91946224+riho5@users.noreply.github.com>
2023-07-26 19:07:37 +0900

    edit js

---
+++ script.js
@@ -0,0 +1,10 @@
+//textの色を変更する
+function changetxtColor(idname,opt="on"){
+    //var obj = document.getElementById(idname);
+    //文字をwhiteに変更する
+    if (opt == "on"){
+        document.getElementById(idname).style.color = '#ffffff';
+    } else{
+        document.getElementById(idname).style.color = '#000000';
+    }
+}
